638d7d4b410887e059fe682bb453bac5243f93e4
JakobPolzl <poe20361@spengergasse.at>
2022-12-03 13:29:02 +0100

    uploaded files

--- DBI/SQL/Lab06/Lab06.sql
+++ DBI/SQL/Lab06/Lab06.sql
@@ -1,21 +1,32 @@
+
 --1
 CREATE OR REPLACE PROCEDURE Gehaltserhoehung (p_erhoehung DECIMAL, p_mitarbeiterID employees.employee_id%TYPE)
 AS 
 BEGIN
+    Select 
+    CASE
+            WHEN  > 7 THEN
+                dbms_output.Put_line('25');
+            WHEN v_jahr > 4 THEN
+                dbms_output.Put_line('25');
+            ELSE
+                dbms_output.Put_line('9');
+    END CASE;
   UPDATE employees
   SET employees.salary = employees.salary * p_erhoehung
   WHERE employees.employee_id like p_mitarbeiterID;
 END;
 
 DECLARE
-    v_jahr INTEGER := ;
+    v_mId employees.employee_id%TYPE :=
+    v_jahr INTEGER := 0;
     BEGIN
         CASE
-            WHEN v_jahr < 5 THEN
-                
-            WHEN v_jahr < 20 THEN
-                dbms_output.Put_line('Cold');
+            WHEN v_jahr > 7 THEN
+                dbms_output.Put_line('25');
+            WHEN v_jahr > 4 THEN
+                dbms_output.Put_line('25');
             ELSE
-                dbms_output.Put_line('Warm');
+                dbms_output.Put_line('9');
         END CASE;
 END;
